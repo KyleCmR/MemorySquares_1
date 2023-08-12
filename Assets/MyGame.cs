3d895f666df756b39ece1e28d083b2511d0db0af
Илья Галкин <galkincmr@gmail.com>
2023-08-12 22:03:19 +0300

    optimizate

--- Assets/MyGame.cs
+++ Assets/MyGame.cs
@@ -14,7 +14,7 @@ public class MyGame : MonoBehaviour
     public float lightUpDuration = 1.5f; // Продолжительность подсветки в секундах
     public float timeBetweenColors = 0.5f; // Время между подсветками в секундах
     private int currentIndex = 0; // Индекс текущего объекта для подсветки
-    
+
     private bool playerInputEnabled = false;
     private bool isGameOver = false;
 
@@ -48,26 +48,25 @@ public class MyGame : MonoBehaviour
 
     private void ClickSquare()
     {
-
         if (Input.GetMouseButtonDown(0) && !isGameOver && playerInputEnabled)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
             {
-                GameObject clickedObject = hit.collider.gameObject; 
+                GameObject clickedObject = hit.collider.gameObject;
                 int index = Array.IndexOf(_colorObjects, clickedObject);// записываем объект на который нажали
-                
+
                 if (CompareWithSequence(index))
                 {
                     currentIndex++;
-                    LightUpObject(index);
-                    _playAudio.PlayClip(3);
-                    
+                    LightUp(index);
+                    _playAudio.PlayClip(0);
+
                     if (currentIndex == sequenceLength)
                     {
                         sequenceLength++;
-                        _playAudio.PlayClip(4);
+                        _playAudio.PlayClip(2);
                         currentIndex = 0;
                         RandomCubes();
                         StartCoroutine(PlaySequence());
@@ -76,7 +75,7 @@ public class MyGame : MonoBehaviour
                     }
                 }
                 else
-                {               
+                {
                     _playAudio.PlayClip(1);
                     _losePanel.SetActive(true);
                     isGameOver = true;
@@ -96,7 +95,7 @@ public class MyGame : MonoBehaviour
             if (index == order)
             {
                 return true;
-            }    
+            }
         }
         return false;
     }
@@ -112,39 +111,36 @@ public class MyGame : MonoBehaviour
 
     void CheckStatus()
     {
-        if (playerInputEnabled == false)
-        {
-            _status.text = "Запоминай";
-            _status.color = Color.red;
-        }
-        else
-        {
-            _status.text = "Повтори";
-            _status.color = Color.green;
-        }
+        _status.text = !playerInputEnabled 
+            ? "Запоминай" 
+            : "Повтори";
+
+        _status.color = !playerInputEnabled 
+            ? Color.red 
+            : Color.green;
+
         if (isGameOver)
         {
             _status.text = "ЛОШАРА";
             _status.color = Color.red;
-        }    
+        }
     }
 
-    void LightUpObject(int index)
+    void LightUp(int index)
     {
         _colorObjects[index].GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
-        StartCoroutine(DelayedTurnOff(index, 0.5f));
-
+        StartCoroutine(LightDown1sec(index, 0.5f));
     }
 
-    void TurnOffObject(int index)
+    void LightDown(int index)
     {
         _colorObjects[index].GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
     }
 
-    IEnumerator DelayedTurnOff(int index, float delay) // Корутина для вызова метода TurnOffObject через задержку
+    IEnumerator LightDown1sec(int index, float delay) // Корутина для вызова метода LightDown через задержку
     {
         yield return new WaitForSeconds(delay);
-        TurnOffObject(index);
+        LightDown(index);
     }
 
     IEnumerator PlaySequence() // Подсветка объектов по порядку из массива sequence
@@ -153,11 +149,11 @@ public class MyGame : MonoBehaviour
         foreach (int index in _sequence)
         {
             yield return new WaitForSeconds(0.5f);
-            LightUpObject(index);
-            _playAudio.PlayClip(2);
+            LightUp(index);
+            _playAudio.PlayClip(3);
             yield return new WaitForSeconds(lightUpDuration); // Продолжительность подсветки в секундах
-            TurnOffObject(index);
-            yield return new WaitForSeconds(timeBetweenColors);     
+            LightDown(index);
+            yield return new WaitForSeconds(timeBetweenColors);
         }
         yield return playerInputEnabled = true;
     }
--- Assets/PlayAudio.cs
+++ Assets/PlayAudio.cs
@@ -5,22 +5,10 @@ using UnityEngine;
 public class PlayAudio : MonoBehaviour
 {
     public AudioSource _audioSource;
-    public AudioClip _audioClip1;
-    public AudioClip _audioClip2;
-    public AudioClip _audioClip3;
-    public AudioClip _audioClip4;
+    [SerializeField] public AudioClip[] _audioClip;
+
     public void PlayClip(int index)
     {
-        switch (index)
-        {
-            case 1:
-                _audioSource.PlayOneShot(_audioClip1); break;
-            case 2:
-                _audioSource.PlayOneShot(_audioClip2); break;
-            case 3:
-                _audioSource.PlayOneShot(_audioClip3); break;
-            case 4:
-                _audioSource.PlayOneShot(_audioClip4); break;
-        }
+        _audioSource.PlayOneShot(_audioClip[index]);
     }
 }
